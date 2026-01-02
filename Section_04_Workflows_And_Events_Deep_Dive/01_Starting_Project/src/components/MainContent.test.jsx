4e53ac69dcd0020f31cf05ffbebbbe1b2500f8a2
Anuroop Pamarthi <Anuroop@DESKTOP-JLDIC5L>
2026-01-02 09:03:13 +0530

    Uploading all section files of Github Actions course

---
+++ Section_04_Workflows_And_Events_Deep_Dive/01_Starting_Project/src/components/MainContent.test.jsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import MainContent from './MainContent';
+
+
+// Added comments to skip the workflow run: [skip ci] /[skip actions]
+
+describe('MainContent', () => {
+  it('should render a button', () => {
+    render(<MainContent />);
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should show the help area after clicking the button', async () => {
+    render(<MainContent />);
+
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+  });
+});
